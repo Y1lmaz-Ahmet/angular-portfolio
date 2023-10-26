08d4f0aad2423f02ca6761157a3babf0f9fc7a13
y1lmaz-ahmet <y.ahmet@outlook.be>
2023-10-26 15:55:27 +0200

    thumbnails

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -7,4 +7,54 @@ import { Component } from '@angular/core';
 })
 export class GalleryComponent {
 
+  galleryItems = [
+    {
+      imageSrc: '../../assets/fintrackr.jpg', 
+      title: 'Data Analytics website UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/FinTrackr',
+      livePreviewLink: 'https://vermillion-lolly-636951.netlify.app/',
+    },
+    {
+      imageSrc: '../../assets/furniture.jpg', 
+      title: 'e-commerce furniture website UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/website-design/tree/main/website-design-15',
+      livePreviewLink: 'https://website-design-7kge.vercel.app/',
+    },
+    {
+      imageSrc: '../../assets/restaurant.jpg', 
+      title: 'Restaurant website UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/website-design/tree/main/website-design-16',
+      livePreviewLink: 'https://y1lmaz-ahmet-restaurant-website.vercel.app',
+    },
+    {
+      imageSrc: '../../assets/snapmaster.png',
+      title: 'Website Design',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/snapmaster',
+      livePreviewLink: 'https://snapmaster.vercel.app',
+    },
+    {
+      imageSrc: '../../assets/coffee-shop.jpg', 
+      title: 'Coffee shop website UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/portfolio-project-coffee-shop',
+      livePreviewLink: 'https://portfolio-project-coffee-shop.vercel.app',
+    },
+    {
+      imageSrc: '../../assets/travel-agency.jpg', 
+      title: 'Travel Agency website UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/website-design/tree/main/website-design-14',
+      livePreviewLink: 'https://website-design-ruby.vercel.app/',
+    },
+    {
+      imageSrc: '../../assets/nexa.jpg', 
+      title: 'Website Design',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/nexa',
+      livePreviewLink: 'https://wondrous-macaron-acf1a4.netlify.app/',
+    },
+    {
+      imageSrc: '../../assets/dashboard.jpg', 
+      title: 'Modern Dashboard UI',
+      githubLink: 'https://github.com/Y1lmaz-Ahmet/react-dashboard-ay',
+      livePreviewLink: 'https://radiant-horse-dee879.netlify.app',
+    },
+  ];
 }
